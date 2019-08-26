2857e6f1755780482ad9d4addce24b993a2ea801
newbalancem5 <newbalancem5@gmail.com>
2019-08-26 20:54:33 +0300

    gg

---
+++ precache-manifest.2c018ef52b4e428845596cc85dd4308c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90dcce88d02be0d7738e347fdb2957c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8674f5f4703c5f63e5b",
+    "url": "/static/css/main.b802ceee.chunk.css"
+  },
+  {
+    "revision": "11f907db78a46b3a80ee",
+    "url": "/static/js/2.f9d43bd6.chunk.js"
+  },
+  {
+    "revision": "f8674f5f4703c5f63e5b",
+    "url": "/static/js/main.745316b4.chunk.js"
+  },
+  {
+    "revision": "04a719eb4a45bf3c4cfc",
+    "url": "/static/js/runtime~main.6fa3b96c.js"
+  }
+]);
